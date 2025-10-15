6bf574e5a6b29b2dfcd61b37b6be88aa1b8f2243
Roberto-projetos <robertosalgado222@gmail.com>
2025-10-15 15:40:12 +0000

    if if-else inceridos trabalho finalizado.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,14 +2,14 @@
 
 int main(){
 
- // 1ª carta
+ // 1ª cartadensidade1, pibpercapta1;
     char codigo1[4], cidade1[30], estado1[30];
     int populacao1, pontosTuristicos1;
-    float area1, pib1;
+    float area1, pib1, densidade1, pibPerCapta1;
 // 2ª carta
     char codigo2[4], cidade2[30], estado2[30];
     int populacao2, pontosTuristicos2;
-    float area2, pib2;
+    float area2, pib2, densidade2, pibPerCapta2;
 // 1ª carta p"rintf e scanf
     printf("Cadastro da primeira carta:\n");
       printf("Digite o código da carta (ex: A01): ");
@@ -26,6 +26,11 @@ int main(){
       scanf("%f", &pib1);
       printf("Digite o número de pontos turísticos: ");
       scanf("%d", &pontosTuristicos1);
+
+      // Calcula densidade populacional e PIB per capita da 1ª carta
+      densidade1 = (area1 > 0) ? (populacao1 / area1) : 0;
+      pibPerCapta1 = (populacao1 > 0) ? ((pib1 * 1000000000) / populacao1) : 0;
+
  // 2ª carta printf e scanf
     printf("Cadastro da segunda carta:\n");
       printf("Digite o código da carta (ex: B02): ");
@@ -41,7 +46,12 @@ int main(){
       printf("Digite o PIB (em bilhões): ");
       scanf("%f", &pib2);
       printf("Digite o número de pontos turísticos: ");
-      scanf("%d", &pontosTuristicos2);
+      scanf("%d", &pontosTuristicos2); 
+
+      // Calcula densidade populacional e PIB per capita da 2ª carta
+      densidade2 = (area2 > 0) ? (populacao2 / area2) : 0;
+      pibPerCapta2 = (populacao2 > 0) ? ((pib2 * 1000000000) / populacao2) : 0;
+ 
  // 1ª carta e seus dados
     printf("Dados da Primeira Carta\n");
       printf("Código: %s\n", codigo1);
@@ -51,6 +61,9 @@ int main(){
       printf("Área: %.2f km²\n", area1);
       printf("PIB: %.2f bilhões\n", pib1);
       printf("Pontos Turísticos: %d\n", pontosTuristicos1);
+      printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+      printf("PIB per capita: %.2f\n", pibPerCapta1);
+ 
  // 2ª carta e seus dados
     printf("Dados da Segunda Carta\n");
       printf("Código: %s\n", codigo2);
@@ -60,6 +73,14 @@ int main(){
       printf("Área: %.2f km²\n", area2);
       printf("PIB: %.2f bilhões\n", pib2);
       printf("Pontos Turísticos: %d\n", pontosTuristicos2);
+      printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+      printf("PIB per capita: %.2f\n", pibPerCapta2);
 
+      if( populacao1 > populacao2){
+       printf(" População da 1ª carta é maior.\n");
+    } else {
+      printf("População da 2ª carta é maior.\n");
+    }
+  
     return 0;
 }  
